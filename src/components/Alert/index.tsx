9a42b92f6e069d3e60d099c1f5de8a8a7f2acb80
Tiago Persch <thiagopersch@gmail.com>
2023-10-12 21:43:19 -0400

    Modifications in styles and new features in component Alert

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { X } from "@styled-icons/feather";
+import { ReactNode, useState } from "react";
 import { DefaultTheme } from "styled-components";
 import * as S from "./styles";
 
@@ -6,14 +7,13 @@ export type AlertProps = {
   children?: string | ReactNode;
   description?: string | ReactNode;
   action?: ReactNode;
-  closeIcon?: ReactNode;
+  closeIcon?: boolean;
   color?: keyof DefaultTheme["colors"];
   backgroundColor?: keyof DefaultTheme["colors"];
   icon?: ReactNode;
-  onClose?: (event: boolean) => void;
-  variant?: "filled" | "outlined" | "standard";
   dark?: boolean;
   iconAnimation?: boolean;
+  bold?: boolean;
 };
 
 const Alert = ({
@@ -24,30 +24,49 @@ const Alert = ({
   color,
   backgroundColor,
   icon,
-  onClose,
-  variant = "filled",
   dark = false,
   iconAnimation = false,
+  bold,
 }: AlertProps) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
   return (
-    <S.Wrapper
-      action={action}
-      closeIcon={closeIcon}
-      backgroundColor={backgroundColor}
-      color={color}
-      hasIcon={!!icon}
-      onClose={onClose}
-      variant={variant}
-      dark={dark}
-      iconAnimation={iconAnimation}
-    >
-      <S.Icon>{!!icon && icon}</S.Icon>
-      <S.TitleDescription>
-        {!!children && <S.Title>{children}</S.Title>}
-        {!!description && <S.Description>{description}</S.Description>}
-      </S.TitleDescription>
-      {!!closeIcon && <div>{closeIcon}</div>}
-    </S.Wrapper>
+    <>
+      {isVisible && (
+        <S.Wrapper
+          action={action}
+          closeIcon={closeIcon}
+          backgroundColor={backgroundColor}
+          color={color}
+          hasIcon={!!icon}
+          dark={dark}
+          iconAnimation={iconAnimation}
+        >
+          {!!icon && <S.Icon>{icon}</S.Icon>}
+          <S.Container>
+            <S.TitleDescription>
+              {!!children && <S.Title bold={bold}>{children}</S.Title>}
+              {!!description && <S.Description>{description}</S.Description>}
+            </S.TitleDescription>
+            {!!closeIcon && (
+              <S.ActionButtonsContainer>
+                <S.ActionButton
+                  color="white"
+                  background="primary"
+                  onClick={handleClose}
+                >
+                  <X />
+                </S.ActionButton>
+              </S.ActionButtonsContainer>
+            )}
+          </S.Container>
+        </S.Wrapper>
+      )}
+    </>
   );
 };
 
--- src/components/Alert/styles.ts
+++ src/components/Alert/styles.ts
@@ -1,4 +1,5 @@
-import { darken } from "polished";
+import theme from "@/styles/theme";
+import { darken, setLightness } from "polished";
 import styled, { DefaultTheme, css } from "styled-components";
 import { AlertProps } from ".";
 
@@ -12,11 +13,13 @@ type WrapperProps = {
   | "dark"
   | "description"
   | "icon"
-  | "variant"
-  | "onClose"
   | "backgroundColor"
   | "iconAnimation"
 >;
+
+type alertModifiersProps = {
+  color: keyof typeof theme.colors;
+};
 
 const alertModifiers = {
   withIcon: (theme: DefaultTheme) => css`
@@ -40,12 +43,11 @@ export const Wrapper = styled.div<WrapperProps>`
     hasIcon,
   }) => css`
     display: ${theme.layout.display.flex};
-    /*justify-content: ${theme.layout.justifyContent.spaceBetween};*/
-    align-items: ${theme.layout.alignItems.center};
     color: ${dark ? darken(0.2, theme.colors[color]) : theme.colors[color]};
     background-color: ${dark
       ? darken(0.2, theme.colors[backgroundColor])
       : theme.colors[backgroundColor]};
+    align-items: ${theme.layout.alignItems.center};
     padding: ${theme.spacings.xsmall};
     transition: ${theme.transitions.fast};
     border-radius: 0.6rem;
@@ -58,26 +60,73 @@ export const Icon = styled.div`
   ${({ theme }) => css`
     display: ${theme.layout.display.flex};
     justify-content: ${theme.layout.justifyContent.center};
-    align-items: ${theme.layout.alignItems.center};
+    align-items: ${theme.layout.alignItems.start};
     margin-right: ${theme.spacings.xsmall};
   `}
 `;
 
-export const TitleDescription = styled.div`
-  ${({ theme }) => css`
-    display: ${theme.layout.display.flex};
-    flex-direction: ${theme.layout.flexDirection.column};
-  `}
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 
-export const Title = styled.span`
-  ${({ theme }) => css`
-    padding: ${theme.spacings.xxsmall} 0;
+export const TitleDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+type TitleProps = {
+  color?: keyof typeof theme.colors;
+  bold?: boolean;
+};
+
+export const Title = styled.span<TitleProps>`
+  ${({ theme, color = "white", bold }) => css`
+    color: ${theme.colors[color]};
+    font-weight: ${bold ? theme.fonts.weight.bold : theme.fonts.weight.normal};
+    padding: ${theme.spacings.xxsmall};
   `}
 `;
 
 export const Description = styled.span`
   ${({ theme }) => css`
-    padding: ${theme.spacings.xxsmall} 0;
+    padding: ${theme.spacings.xxsmall};
+  `}
+`;
+export const ActionButtonsContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${theme.spacings.xxsmall};
+  `}
+`;
+
+type ActionButtonProps = {
+  color: keyof typeof theme.colors;
+  background: keyof typeof theme.colors;
+};
+export const ActionButton = styled.button<ActionButtonProps>`
+  ${({ theme, color, background }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.colors.transparent};
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    border: 0;
+    outline: 0;
+    stroke-width: 2;
+    color: ${theme.colors[color]};
+    padding: 0.4rem;
+    transition: ${theme.transitions.fast};
+
+    &:hover {
+      color: ${theme.colors.white};
+      background-color: ${setLightness(0.5, theme.colors[background])};
+    }
   `}
 `;
